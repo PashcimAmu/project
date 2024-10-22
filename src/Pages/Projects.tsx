bab20a983dea6435a9d2318f622bfb492f5952f7
pashcimamu <pashcimamura@gmail.com>
2024-10-22 15:34:46 +0600

    add projects

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -7,9 +7,22 @@ import { data } from "@/data/data";
 const Projects = () => {
     const mydata= data
   return (
+    <div>
+        <div className="relative flex items-center justify-center mb-4 mt-10">
+          <h1
+            className="text-9xl text-white uppercase"
+            style={{ WebkitTextStroke: "1px #dee2e6" }}
+          >
+            Projects
+          </h1>
+          <h1 className="absolute text-4xl uppercase text-blue-400">
+            My Projects
+          </h1>
+        </div>
     <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-[1200px] mx-auto">
        {mydata.map((item: any)=><Card key={item._id} project={item}/>)
       }
+    </div>
     </div>
   );
 };
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,23 @@
-
-
-
-const Card = ({project}) => {
-    console.log(project);
-    
-
- 
-  return (
-    <div className="shadow-custom hover:shadow-customHover m-5 mb-5 flex w-96 flex-col items-center space-y-5 rounded-2xl border-2 border-black bg-white p-5 transition-all hover:-translate-y-0.5 dark:bg-[#1a1a1a]">
-      
-          <img
-            className="h-64 w-96 border-8 border-gray-50 object-cover transition-all dark:border-[#0d0d0d]"
-            src={project.imgUrl}
-            alt=""
-          />
-        
-      <div className="content">
-        <h2 className="title mb-2 text-center text-2xl font-bold">{project.title}</h2>
-        <p className="description text-center text-base">{project.description}</p>
+const Card = ({ project }) => {
+    return (
+      <div
+        className="shadow-custom m-5 mb-5 flex w-96 flex-col items-center space-y-5 rounded-2xl border-2 border-black bg-white p-5 transition-transform duration-300 ease-in-out hover:translate-y-[-5px] hover:scale-105 hover:shadow-lg dark:bg-[#1a1a1a]"
+        data-aos="fade-up"
+        data-aos-duration="700"
+        data-aos-offset="100"
+      >
+        <img
+          className="h-64 w-96 border-8 border-gray-50 object-cover transition-transform duration-300 ease-in-out hover:scale-105 dark:border-[#0d0d0d] "
+          src={project.imgUrl}
+          alt=""
+        />
+        <div className="content">
+          <h2 className="title mb-2 text-center text-2xl font-bold">{project.title}</h2>
+          <button className="bg-blue-500 w-full mx-2 px-4 py-2 rounded mt-2 hover:bg-blue-400 transition">See Details</button>
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default Card;
+    );
+  };
+  
+  export default Card;
+  
